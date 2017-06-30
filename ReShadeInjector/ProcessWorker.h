159fa2f5c4c242ae68b4f90290d266fb65bb175c
JustasMasiulis <justas.masiulis@yahoo.com>
2017-06-30 13:48:50 +0300

    fixed the bugs and made the code a bit better...

--- ReShadeInjector/ProcessWorker.h
+++ ReShadeInjector/ProcessWorker.h
@@ -1,10 +1,39 @@
 #pragma once
+#include <cstddef> // std::size_t
 #include "InjectionStatus.h"
-#include <windows.h>
-#include <TlHelp32.h>
 
 namespace ProcessWorker
 {
-    extern auto GetProcessId(const wchar_t* szProcessName) -> int;
-    extern auto InjectToProcess(const wchar_t* szProcessName, wchar_t* szLibraryName) -> InjectionStatus;
+    /**
+    * \param szProcessName Name of the process.
+    *
+    * \return Process ID.
+    */
+    auto GetProcessId(const wchar_t* szProcessName) -> int;
+
+    /**
+    * \param szProcessName Name of the process.
+    * \param szLibraryName Name of the library to be injected.
+    *
+    * \return Injection status.
+    */
+    auto InjectToProcess(const wchar_t* szProcessName, const wchar_t* szLibraryName) -> InjectionStatus;
+}
+
+namespace Utils
+{
+    /**
+     * \param pBuf Buffer for the directory to be copied into
+     * \param nSize Size of the buffer
+     * 
+     * \return The number of characters copied into the buffer. 0 on failure
+     */
+    auto GetCurrentWorkingDirectory(wchar_t* pBuf, std::size_t nSize) -> int;
+
+    /**
+     * \brief Waits for user to press any key
+     * 
+     * \param message The message to be displayed
+     */
+    auto WaitForInput(const char* message) -> void;
 }
